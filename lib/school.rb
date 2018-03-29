87bb6841c2e6f3bf0ece7a9a47bbc55d6f2ed044
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-29 21:25:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,4 +5,22 @@ class School
     @name = name
     @roster = {}
   end
+
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
+
+  def grade(level)
+    roster.detect do |x, y|
+    if x == level
+      return y
+    end
+  end
+  def sort
+    new_hash = {}
+    roster.each do |x, y|
+      new_hash[x] = y.sort
+  end
+  new_hash  
 end
