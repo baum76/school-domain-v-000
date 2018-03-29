87e8591d3ad883ead118b7880f08ee6e74abd67a
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-29 21:27:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,9 +13,10 @@ class School
 
   def grade(level)
     roster.detect do |x, y|
-    if x == level
-      return y
-    end
+      if x == level
+        return y
+      end
+    end  
   end
 
   def sort
